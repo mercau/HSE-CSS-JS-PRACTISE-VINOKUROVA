3884b76f4753153824d9da495ed0cd819418c266
mercau <32009632+mercau@users.noreply.github.com>
2018-03-17 15:10:49 +0300

    Scroll

--- javascripts/scroll.js
+++ javascripts/scroll.js
@@ -1,15 +1,30 @@
 $(function() {
   const STATES = [0,1,2,3];
+  let timer;
 
-  function randomState() {
-    return STATES[Math.floor(Math.random() * 4)]
-  }
+  function randomState(exclude) {
+   let possibleStates = $.grep(STATES, function(el) {
+      return el != exclude
+    });
+
+
+    return possibleStates[Math.floor(Math.random() * possibleStates.length)]
+  };
+
+
+  function randomizeState() {
+    let screen = $('.welcomeScreen');
+    let currentState = screen.attr('data-state');
+    console.log(currentState);
+    screen.attr('data-state', randomState(currentState));
+  };
 
   function animateUi() {
-    let screen = $ ('.welcomeScreen');
-    screen.attr('data-state', randomState());
-
+    clearTimeout(timer);
+    timer = setTimeout(randomizeState, 100)
   };
+
+
 
   $(".main").onepage_scroll({
     sectionContainer: ".page",     // sectionContainer accepts any kind of selector in case you don't want to use section
